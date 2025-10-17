77b56714b4b93e7d67e498d7268c1ffbd193dce0
Damian Parjaszewski <dparjaszewski@sii.pl>
2025-10-17 18:06:17 +0200

    Zadania

--- Zadania.py
+++ Zadania.py
@@ -1,4 +1,26 @@
 def Zadanie_1():
     for i in range(1,101): #Jak zrobić tak aby tutaj dać 100 a nie 101?
         print(i)
-Zadanie_1()
+def Zadanie_2():
+    for i in range(1,101,2):
+        print(i)
+def Zadanie_3():
+    for i in range(-1,-101,-1):
+        print(i)
+def Zadanie_4():
+    for i in range(-1,-101,-3):
+        print(i)
+def Zadanie_5():
+    for i in range(100,0,-1):
+        print(i)
+def Zadanie_6():
+    for i in range(1000,10,-10):
+        print(i)
+def Zadanie_7():
+    for i in range(0,101):
+        if (i % 3) == 0:
+            print(i)
+def Zadanie_8():
+    for i in range(0,101):
+        if (i % 2 == 0):
+            print(i)
