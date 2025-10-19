3996c3d8630b3126b2abe53d0d484c8c710d6635
Damian Parjaszewski <dparjaszewski@sii.pl>
2025-10-19 12:16:00 +0200

    Zadania = 20

--- Zadania.py
+++ Zadania.py
@@ -1,3 +1,6 @@
+#Lista do zadań
+array_1 = [1, 2, 4, 5, 4, 3, 2, 5, 1, 10, 7, 5, 3, 4, 5, 1, 12, 3]
+
 def Zadanie_1():
     for i in range(1,101): #Jak zrobić tak aby tutaj dać 100 a nie 101?
         print(i)
@@ -24,3 +27,50 @@ def Zadanie_8():
     for i in range(0,101):
         if (i % 2 == 0):
             print(i)
+def Zadanie_9():
+        for x in range(1,11):
+            for y in range(0,10):
+                print(y+x, end="  ") #Do omówienia
+            print()
+def Zadanie_10():
+    array = []
+    for x in range (1,11):
+        array.append(x**2)
+    print(array)
+def Zadanie_11():
+    for i in range (1,11):
+        if i == 3 or i == 7:
+            continue
+        print(i)
+def Zadanie_12():
+    array = []
+    for x in range (1,101):
+        array.append(0)
+    print(array)
+    print(len(array))
+## Zadanie_13 w ramach comprehenstion
+def Zadanie_14():
+    array =  list(range(1,101))
+    print(array)
+def Zadanie_15():
+    print(array_1[-3])
+def Zadanie_16():
+    print(array_1)
+    myArray = array_1
+    myArray[8] = 3
+    print(myArray)
+def Zadanie_17():
+    if 10 in array_1:
+        return print(True)
+    else:
+        return print(False)
+def Zadanie_18():
+    print(array_1.count(5))
+def Zadanie_19():
+    array_1.sort(reverse=False)
+    print(array_1)
+    array_1.sort(reverse=True)
+    print(array_1)
+def Zadanie_20():
+    print(max(array_1))
+    print(min(array_1))
